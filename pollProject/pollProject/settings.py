6823b9896eece2dc8d34f76db9bdabc4288641ed
YENG-SEBASTIAN <yengaebastian1@gmail.com>
2023-08-25 03:52:10 -0700

    venv

--- pollProject/pollProject/settings.py
+++ pollProject/pollProject/settings.py
@@ -78,20 +78,20 @@ WSGI_APPLICATION = 'pollProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
     'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'PollProject',
-    'USER': 'sabs123', 
-    'PASSWORD': 'localhost', 
-    'HOST': 'localhost',      
-    'PORT': '5432',           
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
+    # 'default': {
+    # 'ENGINE': 'django.db.backends.postgresql',
+    # 'NAME': 'PollProject',
+    # 'USER': 'sabs123', 
+    # 'PASSWORD': 'localhost', 
+    # 'HOST': 'localhost',      
+    # 'PORT': '5432',           
 }
-}
+
 
 
 # Password validation
